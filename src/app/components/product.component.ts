284092e71ccb7d55d791cdbe2142398fed2f9eda
Fernando Robles <ferroblesh@gmail.com>
2020-12-01 22:00:47 -0600

    Lifecycle

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,14 +1,46 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
   @Input() product: Product;
   @Output() productClick: EventEmitter<any> = new EventEmitter();
+
+
+  constructor() {
+    console.log('1.- constructor');
+  }
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log(changes);
+  //   console.log('2.- ngOnChanges');
+  // }
+
+  ngOnInit(): void {
+    console.log('3.- ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('4.- ngDoCheck');
+  }
+
+  ngOnDestroy(): void {
+    console.log('5.- destroy');
+  }
 
   // tslint:disable-next-line: typedef
   addCart() {
