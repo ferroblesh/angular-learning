2e6c2ca91c3944fa41beb3de65028eaacc1ec41e
Fernando Robles <ferroblesh@gmail.com>
2020-12-12 20:44:19 -0600

    Product form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,11 +8,12 @@ import { MaterialModule } from '../material/material.module';
 import { NavComponent } from './components/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { FormProductComponent } from './components/form-product/form-product.component';
 
 
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent, ProductsListComponent],
+  declarations: [ProductFormComponent, NavComponent, ProductsListComponent, FormProductComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
