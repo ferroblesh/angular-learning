42b0cbc16096cf5e508c7911c02f4e6f5979221b
Fernando Robles <ferroblesh@gmail.com>
2020-12-12 23:12:24 -0600

    Using rxjs pipe

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CartService } from 'src/app/core/services/cart.service';
 
 @Component({
@@ -8,15 +10,14 @@ import { CartService } from 'src/app/core/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total: Number = 0;
+  total$: Observable<number>;
 
   constructor(
     private cartService: CartService
   ) {
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    })
+    this.total$ = this.cartService.cart$.pipe(
+      map(products => products.length)
+    );
   }
 
   ngOnInit() {
