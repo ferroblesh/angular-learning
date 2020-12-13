86124fc95f2fd3135e446dad8f5e0fa5f1fa7fde
Fernando Robles <ferroblesh@gmail.com>
2020-12-12 23:07:09 -0600

    Using rxjs

--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -6,6 +6,7 @@ import {
   OnInit,
   OnDestroy
 } from '@angular/core';
+import { CartService } from 'src/app/core/services/cart.service';
 
 import { Product } from '../../../core/models/product.model';
 
@@ -21,7 +22,9 @@ export class ProductComponent implements OnInit, OnDestroy {
 
   today = new Date();
 
-  constructor() {
+  constructor(
+    private cartService: CartService
+  ) {
     console.log('1. constructor');
   }
 
@@ -36,7 +39,7 @@ export class ProductComponent implements OnInit, OnDestroy {
 
   addCart() {
     console.log('añadir al carrito');
-    this.productClicked.emit(this.product.id);
+    this.cartService.addCart(this.product);
   }
 
 }
